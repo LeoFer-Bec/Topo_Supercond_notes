5a3a15962a20289fe130d33f52f324f16f518ad5
LeoFer-Bec <victor_f_becerra@outlook.com>
2023-10-21 17:17:04 +0200

    alpha version entropy notebook

---
+++ data/entropy/V_0_1.8/Ln_950/submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH -J V0_1.8_Ln950
+#SBATCH -N 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --mem=4000M
+#SBATCH --time=2:35:00
+#SBATCH -A g94-1727
+#SBATCH -p topola
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=becerra@magtop.ifpan.edu.pl
+#SBATCH -e errfile
+    
+source /lu/topola/home/victor/miniconda3/etc/profile.d/conda.sh
+    
+cd /lu/topola/home/victor/entropy/V_0_1.8/Ln_950
+    
+python3.7 entropy_cluster.py
+    
+exit
